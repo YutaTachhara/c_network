57f36d4b529ab0d8b97802e44daac3f39c186116
Yuta Tachihara <>
2020-10-03 20:03:11 +0900

    initial

---
+++ sock_init.c
@@ -0,0 +1,38 @@
+#if defined(_WIN32)
+#if ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_ew.lib")
+
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+
+#endif
+
+#include <stdio.h>
+
+int main() {
+
+#if defined(_Win32)
+    WSÅDATA d;
+    if (WSAStartup(MAKEWORD(2, 2), &d)) {
+        fprintf(stderr, "Failed to initialize.\n");
+        return 1;
+    }
+#endif
+
+    printf("Ready to use socket API.\n");
+
+#if defined(_WIN32)
+    WSACleanup();
+#endif
+
+    return 0;
+}
